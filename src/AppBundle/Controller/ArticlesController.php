7a79581f8ce53e646f9fb15c71b151999f5c1874
Cedrick <pommier.cedrick@gmail.com>
2021-11-29 16:09:07 +0100

    ajout d'un layout admin et creation de la vue

--- src/AppBundle/Controller/ArticlesController.php
+++ src/AppBundle/Controller/ArticlesController.php
@@ -12,44 +12,79 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticlesController extends Controller
 {
     /**
+     * Le formulaire est généré avec Article Type
      * @Route("/Ajout", name="AddArticles")
      */
     public function AjoutAction(Request $request)
     {
         //instance de l'entité
         $produit = new Articles();
-
         
+        // On cree le formulaire
         $form = $this->createForm(ArticlesType::class,$produit);
+        $form->handleRequest($request);
 
         if($form->isSubmitted()) // Deuxième possibilité pour récuperer les informations du formulaire
         {
 
+            // Connexion avec Doctrine et persistance de la table
+            $cnx = $this->getDoctrine()->getManager();
+            $cnx->persist($produit);
+            $cnx->flush();
+
+            return new Response('Ajout Ok');
         }
 
+        $cnx=$this->getDoctrine()->getManager();
+        $articles = $cnx->getRepository(Articles::class)->findAll();
+
         return $this->render('@App/Articles/ajout.html.twig', array(
-            'form'  => $form->createView()
+            'form'  => $form->createView(),
+            'articles'  => $articles
         ));
     }
     
     /**
+     * Le formulaire est ajouté à la main
      * @Route("/Ajout2", name="AddArticles2")
      */
     public function AjoutAction2(Request $request)
     {
+
+
         //instance de l'entité
         $produit = new Articles();
-        $produit->setNom('PC');
-        $produit->setQte(152);
-        $produit->setDescription('PC DELL');
 
-        // Connexion avec Doctrine et persistance de la table
-        $cnx = $this->getDoctrine()->getManager();
-        $cnx->persist($produit);
-        $cnx->flush();
+        if($request->isMethod('POST')){
 
+            $produit->setNom($request->get('nom'));
+            $produit->setQte($request->get('qte'));
+            $produit->setDescription($request->get('desc'));
+    
+            // Connexion avec Doctrine et persistance de la table
+            $cnx = $this->getDoctrine()->getManager();
+            $cnx->persist($produit);
+            $cnx->flush();
+    
+    
+            return new Response('Ajout Ok');
+        }
 
-        return new Response('Ajout Ok');
+        return $this->render('@App/Articles/ajout2.html.twig');
+
+    }
+
+    /**
+     * @Route("/Afficher", name="afficherArticle")
+     */
+    public function AfficherAction(){
+        // Connexion avec Doctrine
+        $cnx=$this->getDoctrine()->getManager();
+        $articles = $cnx->getRepository(Articles::class)->findAll();
+
+        return $this->render('@App/Articles/afficherArticle.html.twig', [
+            'articles'  => $articles
+        ]);
     }
 
 }
